141ccff66b52f36f814d0b4ba4e68b7bcc17e776
githubnguyendang <thienphong440011@gmail.com>
2021-06-04 15:16:25 +0700

    add attr loaigiayphep

--- app/Models/GPNuocMat.php
+++ app/Models/GPNuocMat.php
@@ -11,6 +11,7 @@ class GPNuocMat extends Model
 {
     use HasFactory;
     public $table = "gp_nuoc_mat";
+    protected $appends = ['loaigiayphep'];
 
     public function hang_muc_ct()
     {
@@ -20,5 +21,18 @@ class GPNuocMat extends Model
     public function luu_luong_theo_muc_dich_sd()
     {
         return $this->hasMany(LuuLuongTheoMucDichSD::class, 'id_gp', 'id');
+    }
+
+    public function getLoaigiayphepAttribute()
+    {
+        if($this->loai_gp == "cap-moi"){
+            return "Cấp mới";
+        }else if($this->loai_ct == "cap-lai"){
+            return "Cấp lại";
+        }else if($this->loai_ct == "thu-hoi"){
+            return "Thu hồi";
+        }else{
+            return "";
+        }
     }
 }
